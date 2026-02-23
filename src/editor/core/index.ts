cd1c35bd02ea4dae8d4eeefbcddae154cdf0d7dd
CigThePig <130598968+CigThePig@users.noreply.github.com>
2026-02-23 01:49:57 -0800

    refactor editor top bar to event bus architecture

--- src/editor/core/index.ts
+++ src/editor/core/index.ts
@@ -14,3 +14,6 @@ export {
 export type { EditorMode } from './editorMode';
 
 export { MODE_TO_LAYER, MODE_TO_TOOL, getLegacyState } from './modeMapping';
+
+export { EditorEventBus, editorEventBus } from './eventBus';
+export type { EditorEventMap, EditorModeState } from './eventBus';
